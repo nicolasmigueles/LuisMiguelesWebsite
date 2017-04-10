92d8aab8e70c7e7bc8d46a343ac52b45a0c05162
Nicolás Migueles <nicolasmfotografo@gmail.com>
2017-04-10 16:05:26 -0300

    categorias.class adds

--- core/models/Categorias.Class.php
+++ core/models/Categorias.Class.php
@@ -9,17 +9,19 @@ class Categorias
   public function __construct(){
     $this->db = new Con;
   }
-  public function AddCategoria(){
-
+  public function AddCategoria($nombre){
+    $q = $this->db->query("INSERT INTO categorias SET nombre = $nombre LIMIT 1;");
+    return ($q ? true : false);
   }
-  public function DelCategoria(){
-
+  public function DelCategoria($nombre){
+    $q = $this->db->query("DELETE FROM categorias WHERE nombre = $nombre LIMIT 1;");
+    return ($q ? true : false);
   }
-  public function Categorias(){
-    $q = $this->db->query("SELECT * FROM categorias;")or die(mysqli_error($this->db));
-    $res = $this->db->recorrer($q)['nombre'];// ver porque devuelve una sola row cuando deveria devolver
-    $this->db->liberar($q);
-    return $res;
+  public function ToggleVisibleCategoria($nombre){
+    $qu = $this->db->query("SELECT visible FROM categorias WHERE nombre = $nombre");
+    $new = $this->db->recorrer($qu)[0] == 1 ? 0 : 1;
+    $q = $this->db->query("UPDATE categorias SET visible = $new WHERE nombre = $nombre LIMIT 1;");
+    return ($q ? true : false);
   }
   public function Categoria($id){
     $q = $this->db->query("SELECT nombre FROM categorias WHERE (id=$id)");
@@ -27,9 +29,13 @@ class Categorias
   }
   public function CategoriadelProd($idprod){
     $q = $this->db->query("SELECT categoria FROM productos WHERE (id=$idprod)") or die(mysqli_error($this->db));
-    $id = $this->db->recorrer($q)[0];
-    $q2 = $this->db->query("SELECT nombre FROM categorias WHERE (id=$id)")or die(mysqli_error($this->db));
+    $idcat = $this->db->recorrer($q)[0];
+    $q2 = $this->db->query("SELECT nombre FROM categorias WHERE (id=$idcat)")or die(mysqli_error($this->db));
     return $this->db->recorrer($q2)[0];
+  }
+  public function ArrayCategorias(){
+    $q = $this->db->query("SELECT nombre FROM categorias");
+    return $this->recorrer($q);
   }
 }
 ?>
